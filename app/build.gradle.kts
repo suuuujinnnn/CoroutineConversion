abffbee90f19ab8db034578926c095f8042930c5
suuuujinnnn <suuuujinnnn@gmail.com>
2025-04-02 15:45:55 -0400

    refactor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,8 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -2,16 +2,15 @@ package edu.temple.coroutineconversion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
-
-    //TODO (Refactor to replace Thread code with coroutines)
 
     private val cakeImageView: ImageView by lazy {
         findViewById(R.id.imageView)
@@ -21,24 +20,22 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.currentTextView)
     }
 
-    val handler = Handler(Looper.getMainLooper(), Handler.Callback {
-
-        currentTextView.text = String.format(Locale.getDefault(), "Current opacity: %d", it.what)
-        cakeImageView.alpha = it.what / 100f
-        true
-    })
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.revealButton).setOnClickListener{
-            Thread{
-                repeat(100) {
-                    handler.sendEmptyMessage(it)
-                    Thread.sleep(40)
+        findViewById<Button>(R.id.revealButton).setOnClickListener {
+            lifecycleScope.launch {
+                repeat(100) { progress ->
+                    delay(40)
+                    updateUi(progress)
                 }
-            }.start()
+            }
         }
+    }
+
+    private fun updateUi(progress: Int) {
+        currentTextView.text = String.format(Locale.getDefault(), "Current opacity: %d", progress)
+        cakeImageView.alpha = progress / 100f
     }
 }
